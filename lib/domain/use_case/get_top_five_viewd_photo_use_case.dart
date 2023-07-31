93b805f0d0adad8d4d0d2071280a1fa7d8785aa2
yeonju <97602748+yeondooo@users.noreply.github.com>
2023-07-31 20:28:32 +0900

    feat: result class 적용한 use case 구현

---
+++ lib/domain/use_case/get_top_five_viewd_photo_use_case.dart
@@ -0,0 +1,20 @@
+import 'package:image_search_14day/data/data_source/result.dart';
+import 'package:image_search_14day/domain/model/photo.dart';
+import 'package:image_search_14day/domain/repository/photo_repository.dart';
+
+class GetTopFiveViewdPhotoUseCase {
+  final PhotoRepository _photoRepository;
+
+  GetTopFiveViewdPhotoUseCase(this._photoRepository);
+
+  Future<Result<List<Photo>>> execute(String query) async {
+    try {
+      final photos = await _photoRepository.getPhotos(query);
+      photos.sort((a, b) => -a.views.compareTo(b.views));
+
+      return Result.success(photos.take(5).toList());
+    } catch (err) {
+      return const Result.error('에러가 발생했습니다.');
+    }
+  }
+}
